8c97af3ddef44bf0a209641d4915c9727a12be85
Lucilor <lucilor@me.com>
2018-09-25 11:34:05 +0800

    1.3.1

--- extension.js
+++ extension.js
@@ -66,7 +66,7 @@ game.import("extension",function(lib,game,ui,get,ai,_status){return {name:"Dota2
     };
 },precontent:function (Dota2){
     if(Dota2.enable){
-        if(lib.config.noname_Dota2_version==undefined||lib.config.noname_Dota2_version=='1.0.0.0') game.saveConfig('noname_Dota2_version',"1.0.0");
+        if(lib.config.noname_Dota2_version==undefined||lib.config.noname_Dota2_version=='1.0.0.0') game.saveConfig('noname_Dota2_version',"1.3.1");
         var edit=lib.extensionMenu.extension_Dota2.edit;
         var deletex=lib.extensionMenu.extension_Dota2.delete;
         delete lib.extensionMenu.extension_Dota2.edit;
